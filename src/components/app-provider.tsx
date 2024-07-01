f4a77d66482865658d8e5258eef59f87c9e7866f
Van Hieu <hieuhk195@gmail.com>
2024-07-01 15:59:58 +0700

    handle refresh_token

--- src/components/app-provider.tsx
+++ src/components/app-provider.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import RefreshToken from "./refresh-token";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,7 +20,7 @@ export default function AppProvider({
 }) {
   return (
     <QueryClientProvider client={queryClient}>
-      {children} <ReactQueryDevtools initialIsOpen={false} />
+      {children} <RefreshToken /> <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
